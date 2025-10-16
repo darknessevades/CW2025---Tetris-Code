757911763502ecfe375b87d66baef6e5f06b6704
darknessevades <bovenlee168@outlook.com>
2025-10-16 16:34:00 +0800

    "Removed a redundancy on nextShape in the getNextShape() method."

--- src/main/java/com/comp2042/BrickRotator.java
+++ src/main/java/com/comp2042/BrickRotator.java
@@ -2,29 +2,30 @@ package com.comp2042;
 
 import com.comp2042.logic.bricks.Brick;
 
+// Each tetris piece can have 4 rotation positions, this class checks for the piece's current rotation.
+
 public class BrickRotator {
 
     private Brick brick;
-    private int currentShape = 0;
+    private int currentShape = 0; // default rotation for the tetris piece
 
     public NextShapeInfo getNextShape() {
-        int nextShape = currentShape;
-        nextShape = (++nextShape) % brick.getShapeMatrix().size();
+        int nextShape = (currentShape + 1) % brick.getShapeMatrix().size();
         return new NextShapeInfo(brick.getShapeMatrix().get(nextShape), nextShape);
     }
 
     public int[][] getCurrentShape() {
         return brick.getShapeMatrix().get(currentShape);
-    }
+    } // get the current rotation of brick
 
     public void setCurrentShape(int currentShape) {
         this.currentShape = currentShape;
-    }
+    } // update the rotation index if confirmed to be valid
 
     public void setBrick(Brick brick) {
         this.brick = brick;
         currentShape = 0;
-    }
+    } // set a new piece, reset the rotation index to default
 
 
 }
